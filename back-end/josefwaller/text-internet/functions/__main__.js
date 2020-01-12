f23012d613025370fce37b47eba3c9afc79d80b7
Josef Waller <josef@siriusapplications.com>
2020-01-11 22:02:59 -0800

    Scrape bing for search results

--- back-end/josefwaller/text-internet/functions/__main__.js
+++ back-end/josefwaller/text-internet/functions/__main__.js
@@ -1,14 +1,13 @@
-const lib = require('lib');
-const request = require('request');
 const rp = require('request-promise');
-const xml = require("xml");
+const JSSoup = require('jssoup').default;
+
 
 module.exports = async (name = 'world', context) => {
 
   // Get the response to the request
-  let x = await getResponse();
+  let responce = await getResponse();
   // For now, just return the response
-  return result;
+  return responce;
 };
 
 /*
@@ -16,7 +15,32 @@ module.exports = async (name = 'world', context) => {
  * Returns a json response
  */
 function getResponse(text) {
-  // First,
+  // First, get bing :( response
+  let res = rp(`https://www.bing.com/search?q=beavers`).then((body) => {
+    var soup = new JSSoup(body);
+    // Get the regular responses
+    var resultsHTML = soup.findAll('li', 'b_algo');
+    let results = resultsHTML.map(e => ({
+      title: e.find("h2").text,
+      url: e.find("h2").find("a").attrs["href"],
+      desc: e.find('p') ? e.find("p").text : e.find("span").text
+    }));
+    // Get the card, if there is one
+    let x = soup.find("div", "b_subModule");
+    if (x) {
+      results = results.concat({
+        type: "card",
+        desc: x.find("span").text,
+        url: x.find("a").attrs["href"],
+        title: x.find("h2").text
+      });
+    }
+    return results;
+  });
+  return res;
+}
+
+function getWikipedia(url) {
   let x = rp('https://en.wikipedia.org/w/api.php?action=query&explaintext=1&prop=extracts&titles=fried_chicken&format=json').then((body) => {
     console.log("Responce received");
     // Get the body of the wikipedia article
@@ -28,5 +52,6 @@ function getResponse(text) {
     return response;
   });
   return x;
+
 
 }
