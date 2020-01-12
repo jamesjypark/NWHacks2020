5dc381d333147ecb9bc8fdb7bb740887a84db0df
Juyoung Park <james.park.victoria@gmail.com>
2020-01-11 21:07:19 -0800

    added .keep to directories

---
+++ front-end/Froogle/components/screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+class SearchScreen extends React.Component {
+  state = {};
+  render() {
+    return (
+      <View>
+        <Text>SearchScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default SearchScreen;
