d2c12fc1e1828febcfce3916cb89a6cd65d7576f
nilay-freshworks <nilay@freshworks.io>
2020-01-12 00:20:05 -0800

    Searchbar - Fixed Searchbar UI

--- front-end/Froogle/App.js
+++ front-end/Froogle/App.js
@@ -60,6 +60,7 @@ class App extends React.Component {
         url: 'link 3',
       },
     ];
+    
     return (
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         {results ? (
--- front-end/Froogle/components/common/SearchBar.js
+++ front-end/Froogle/components/common/SearchBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { View, StyleSheet, TextInput, Image, TouchableOpacity } from 'react-native';
 
+import froogleLogo from '../../assets/froogleLogo.png';
 import searchIcon from '../../assets/searchIcon.png';
 import { TEAL_COLOR } from '../../constants/hexcodes';
 
@@ -13,20 +14,41 @@ class SearchBar extends React.Component {
     submitQuery: PropTypes.func.isRequired,
   }
 
+  renderSearchBar = (onChangeQuery, submitQuery) => (
+    <View style={styles.searchBar}>
+      <Image style={styles.searchBarLogo} source={froogleLogo} />
+      <TextInput
+        style={styles.searchBarInput}
+        onChangeText={onChangeQuery}
+        onSubmitEditing={submitQuery}
+      />
+      {/* TODO: Fix search button */}
+      <TouchableOpacity activeOpacity={0.5} onPress={this.test}>
+        <Image style={styles.searchBarIcon} source={searchIcon} />
+      </TouchableOpacity>
+    </View>
+  );
+
+  renderLargeSearchBar = (onChangeQuery, submitQuery) => (
+    <View style={styles.searchBar}>
+      <TextInput
+        style={[styles.searchBarInput, styles.searchBarInputLarge]}
+        onChangeText={onChangeQuery}
+        onSubmitEditing={submitQuery}
+      />
+      {/* TODO: Fix search button */}
+      <TouchableOpacity activeOpacity={0.5} onPress={this.test}>
+        <Image style={[styles.searchBarIcon, styles.searchBarIconLarge]} source={searchIcon} />
+      </TouchableOpacity>
+    </View>
+  );
+
   render() {
     const { isLarge, onChangeQuery, submitQuery } = this.props;
-    const textInputStyles = isLarge
-      ? [styles.searchBarInput, styles.searchBarInputLarge]
-      : styles.searchBarInput;
 
-    return (
-      <View style={styles.searchBar}>
-        <TextInput style={textInputStyles} onChangeText={onChangeQuery} onSubmitEditing={submitQuery}/>
-        <TouchableOpacity activeOpacity={0.5} onPress={this.test}>
-          <Image style={styles.searchBarIcon} source={searchIcon} />
-        </TouchableOpacity>
-      </View>
-    );
+    return isLarge
+      ? this.renderLargeSearchBar(onChangeQuery, submitQuery)
+      : this.renderSearchBar(onChangeQuery, submitQuery)
   }
 }
 
@@ -36,8 +58,15 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     marginHorizontal: 10
   },
+  searchBarLogo: {
+    width: 30,
+    height: 40,
+    marginLeft: -5,
+    marginRight: 10,
+    resizeMode: "contain"
+  },
   searchBarInput: {
-    width: "100%",
+    width: "90%",
     height: 40,
     paddingLeft: 15,
     paddingRight: 45,
@@ -51,8 +80,14 @@ const styles = StyleSheet.create({
   },
   searchBarIcon: {
     width: 30,
+    height: 40,
     marginLeft: -38,
-    marginTop: 9,
+    resizeMode: "contain"
+  },
+  searchBarIconLarge: {
+    width: 30,
+    marginLeft: -38,
+    marginTop: 25,
     resizeMode: "contain"
   }
 });
